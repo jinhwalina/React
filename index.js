03685600486a8b9dbdf4f9b16d6d7863b2195257
jinhwa <slzp0119@naver.com>
2021-01-05 22:29:15 +0900

    js 추가해주기 전까지!

--- index.js
+++ index.js
@@ -1,14 +1,62 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const { User } = require("./models/User");
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+// application/json
+app.use(bodyParser.json()); // 이 부분에 오타가 있었는데 어떻게 에러가 안났을까...
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://jinhwa:wlsghk@reactyoutube.vrhnm.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://jinhwa:wlsghk@reactyoutube.vrhnm.mongodb.net/ReactYoutube?retryWrites=true&w=majority', {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(()=> console.log('MongoDB Connected..'))
 .catch(err => console.log(err))
 
 
-app.get('/', (req, res) => { res.send('안녕하세요 !!') })
+app.get('/', (req, res) => { res.send('안녕하세요 !! 이게 몇번째야!') })
+
+
+app.post('/register', (req, res)=> {
+    // 회원 가입 할때 필요한 정보들을 client에서 가져오면
+    // 그것들을 DB에 넣어준다. 
+
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        // 몽고DB에서 오는 메서드
+        // 이렇게 써주면 위에 있는 정보들이 DB에 저장되는것임.
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
+
+app.post('/login', (req, res) => {
+    User.findOne({ email: req.body.email }, (err,user) => {
+        if(!user){
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다!"
+            })
+        }
+        user.comparePassword(req.body.password , (err, isMatch) => {
+            if(!isMatch)
+                return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다!"})
+            user.generateToken((err, user)=>{
+                if(err) return res.status(400).send(err);
+                // 토큰을 저장한다. 어디에?! 쿠키, 로컬스토리지..등..
+                res.cookie("x_auth", user.token)
+                .status(200)
+                .json({ loginSuccess: true, userId: user._id })
+            })
+        })
+    })
+})
 
 app.listen(port, () => { console.log(`Example app listening at http://localhost:${port}`) })
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10 
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name:{
@@ -31,7 +34,41 @@ const userSchema = mongoose.Schema({
     }
 })
 
+userSchema.pre('save', function( next ){
+    var user = this;
 
+    if(user.isModified('password')){
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err)
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    } else {
+        next() // 비밀번호 말고 다른걸 바꿀 때는 그냥 나갈 수 있도록. 아니면 그냥 여기 갇히게된다.
+    }
+})
+
+userSchema.methods.comparePassword = function(plainPassword,cb){
+    // plain 평범한 암호! 암호화된 비밀번호! 둘이 맞는지 확인을 해야함 
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err),
+        cb(null, isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(cb){
+    var user = this;
+    // jsonwebtoken을 이용해서
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    user.token = token
+    user.save(function(err,user){
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 module.exports = { User }
