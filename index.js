ab0aa9597af721845cf166e726a159240e83cf44
jinhwa <slzp0119@naver.com>
2021-01-06 21:08:29 +0900

    Auth Router

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
 const config = require('./config/dev');
-
+const { auth } = require('./middleware/auth');
 const { User } = require("./models/User");
 
 // application/x-www-form-urlencoded
@@ -24,7 +24,7 @@ mongoose.connect(config.mongoURI, {
 app.get('/', (req, res) => { res.send('안녕하세요 !! 이게 몇번째야!') })
 
 
-app.post('/register', (req, res)=> {
+app.post('/api/users/register', (req, res)=> {
     // 회원 가입 할때 필요한 정보들을 client에서 가져오면
     // 그것들을 DB에 넣어준다. 
 
@@ -40,7 +40,7 @@ app.post('/register', (req, res)=> {
 })
 
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
 
     User.findOne({ email: req.body.email }, (err,user) => {
         if(!user){
@@ -62,5 +62,23 @@ app.post('/login', (req, res) => {
         })
     })
 })
+
+app.get('/api/users/auth', auth , (req, res)=> {
+
+    // 여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 true 라는 말 
+    res.status(200).json({
+        _id: req.user._id,
+        isAdmin: req.user.role === 0 ? false : true,
+        // role 1 Admin     role 2 특정 부서 Admin
+        // role 0 > 일반 유저   role 0 이 아니면 관리자 
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    })
+})
+
 
 app.listen(port, () => { console.log(`Example app listening at http://localhost:${port}`) })
