09c45dbac9aee8cdd8f2e5d16a23041770744404
jinhwa <slzp0119@naver.com>
2021-01-12 14:37:51 +0900

    11일 오류 사항 수정!

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => { res.send('안녕하세요 !! 이게 몇번째야!') })
 
-
 app.post('/api/users/register', (req, res)=> {
     // 회원 가입 할때 필요한 정보들을 client에서 가져오면
     // 그것들을 DB에 넣어준다. 
