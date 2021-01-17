1b2dd35a14e1d645ac53ad07e59448575415e7e5
jinhwa <slzp0119@naver.com>
2021-01-17 11:42:16 +0900

    로그아웃 기능까지 완료!

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,16 +1,42 @@
 import React,{ useEffect } from 'react';
-import axios from 'axios'
-function LandingPage() {
+import axios from 'axios';
+import {withRouter} from 'react-router-dom';
+
+
+function LandingPage(props) {
     useEffect(() => {
         axios.get('/api/hello')
         .then(response => console.log(response.data))
     }, [])
+
+    // 로그아웃을 위한 함수 
+    const onClickHandler = () => {
+        axios.get('/api/users/logout')
+            .then(response => {
+                console.log(1) // 데이터가 애초에 여기까지만 들어옴 ㅠ
+                if (response.data.success) {
+                    console.log(response.data) // data 찍히는것도 확인함! 
+                    if (response.data.success) {
+                        props.history.push('/login')
+                    } else {
+                        alert('로그아웃에 실패하였습니다.')
+                    }
+                }
+            })
+    }
+
+
     return (
         <div style={{ display:'flex', justifyContent:'center', alignItems: 'center'
         , width: '100%', height:'100vh'
         }}>
             <h2>시작페이지</h2>
+
+            <button onClick={onClickHandler}>
+                LOGOUT
+            </button>
+
         </div>
     )
 }
-export default LandingPage
+export default withRouter(LandingPage)
--- server/index.js
+++ server/index.js
@@ -83,10 +83,11 @@ app.get('/api/users/auth', auth , (req, res)=> {
 })
 
 app.get('/api/users/logout', auth, (req, res) => {
-    User.findOneAndUpdate({_id: req.user._id},
-        { token: ""}
+    console.log(1)
+    User.findOneAndUpdate({ _id: req.user._id },
+        { token: "" }
     , (err, user) => {
-        if(err) return res.json({ success: false, err});
+        if (err) return res.json({ success: false, err });
         return res.status(200).send({
             success: true
         })
