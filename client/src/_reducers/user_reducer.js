2f99f610db51bf274b8fc064fb5c7cf72682e553
jinhwa <slzp0119@naver.com>
2021-01-17 10:19:25 +0900

    회원가입까지 완료!

--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -1,6 +1,6 @@
 
 import {
-    LOGIN_USER
+    LOGIN_USER, REGISTER_USER
 } from '../_actions/types';
 
 // eslint-disable-next-line import/no-anonymous-default-export
@@ -10,18 +10,11 @@ export default function (state = {}, action) {
             return{ ...state, loginSuccess: action.payload}
             // eslint-disable-next-line no-unreachable
             break;
+        case REGISTER_USER:
+            return {...state, register: action.payload}
+            // eslint-disable-next-line no-unreachable
+            break;
         default:
            return state;
     }
 }
-
-// export default function (state, action) {
-//     switch (action.type) {
-//         case LOGIN_USER:
-//                 return{...state, loginSuccess: action.payload}
-//             break;
-    
-//         default:
-//             return state;
-//     }
-// }
